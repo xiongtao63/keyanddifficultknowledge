f60655147868ee7e0b910857f53d9bc601778372
xiongtao <xiongtao@sunline.com>
2022-07-21 17:02:11 +0800

    测试FileChannerl

--- app/src/main/java/com/xiongtao/keyanddifficultknowledge/MainActivity.kt
+++ app/src/main/java/com/xiongtao/keyanddifficultknowledge/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.xiongtao.keyanddifficultknowledge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.nio.channels.FileChannel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun test(view: android.view.View) {
+
     }
 
 }
--- app/src/test/java/com/xiongtao/keyanddifficultknowledge/ExampleUnitTest.kt
+++ app/src/test/java/com/xiongtao/keyanddifficultknowledge/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.xiongtao.keyanddifficultknowledge
 
+import android.util.Log
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,37 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    public fun testFileIO() {
+
+        val fis = FileInputStream("F:\\Download\\kougou\\kugou.exe")
+        val fos = FileOutputStream("F:\\Download\\kougou\\kugou1.exe")
+        var len = 0
+        val buffer = ByteArray(4096)
+        while ((fis.read(buffer).also { len = it }) != -1) {
+            fos.write(buffer, 0, len)
+        }
+        fis.close()
+        fos.close()
+
+
+    }
+
+    @Test
+    public fun testFileChannerl() {
+
+        val inChannel = FileInputStream("F:\\Download\\kougou\\kugou.exe").channel
+        val outChannel = FileOutputStream("F:\\Download\\kougou\\kugou2.exe").channel
+
+        outChannel.transferFrom(inChannel, 0, inChannel.size())
+        inChannel.close()
+        outChannel.close()
+
+
     }
 }
